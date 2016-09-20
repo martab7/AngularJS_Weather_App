7d14a384e3318e51608814d6ef7cdc176a2bfb26
Anthony <martab7.am@gmail.com>
2016-09-20 10:41:44 -0500

    Functionality to toggle completed. Just need to display information and stylize.

--- js/weatherApp.js
+++ js/weatherApp.js
@@ -5,23 +5,34 @@
 
     app.controller('WeatherController', function ($scope, $http) {
 
-    $scope.city = 'Austin'
+    var cities = ['Austin', 'Dallas', 'San Antonio']
     $scope.units = 'metric'
 
-    // $scope.toggle = function () {
-      var url = 'http://api.openweathermap.org/data/2.5/forecast'
+    this.toggle = function () {
+      if($scope.units == 'metric') {
+        $scope.units = 'imperial'
+      }else if($scope.units == 'imperial') {
+        $scope.units = 'kelvin'
+      }else if ($scope.units == 'kelvin') {
+        $scope.units = 'metric'
+      }
 
-      $http.jsonp(url, { params : {
-        q : $scope.city,
-        units : $scope.units,
-        appid : '9eee084476f605e9dada18503be885cd',
-        callback : 'JSON_CALLBACK'
-      }}).success(function (data, status, headers, config) {
-        console.log('hi')
-        $scope.main = data.list[0].main.temp
-        console.log($scope.main)
+      cities.forEach(function (city){
+        $scope.city = city
+        var url = 'http://api.openweathermap.org/data/2.5/forecast'
+
+        $http.jsonp(url, { params : {
+          q : $scope.city,
+          units : $scope.units,
+          appid : '9eee084476f605e9dada18503be885cd',
+          callback : 'JSON_CALLBACK'
+        }}).success(function (data, status, headers, config) {
+          $scope.main = data.list[0].main.temp
+          console.log($scope.main)
+        })
       })
-    // }
+      console.log('hi')
+    }
 
   })
 })()
