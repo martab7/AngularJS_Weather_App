cc8e90c2388cff09c7e72ec6e9ab5d47b84bb780
Anthony <martab7.am@gmail.com>
2016-09-20 10:00:11 -0500

    Established connection to api and displaying data in console

--- js/weatherApp.js
+++ js/weatherApp.js
@@ -1,12 +1,27 @@
 "use strict";
 
 (function() {
-    var app = angular.module("weatherApp", []);
+    var app = angular.module('weatherApp', [])
 
-    app.controller("WeahterController", function(){
-      this.showElement = true;
+    app.controller('WeatherController', function ($scope, $http) {
 
-      this.showElement = !this.showElement;
+    $scope.city = 'Austin'
+    $scope.units = 'metric'
 
-    })
-})();
+    // $scope.toggle = function () {
+      var url = 'http://api.openweathermap.org/data/2.5/forecast'
+
+      $http.jsonp(url, { params : {
+        q : $scope.city,
+        units : $scope.units,
+        appid : '9eee084476f605e9dada18503be885cd',
+        callback : 'JSON_CALLBACK'
+      }}).success(function (data, status, headers, config) {
+        console.log('hi')
+        $scope.main = data.list[0].main.temp
+        console.log($scope.main)
+      })
+    // }
+
+  })
+})()
