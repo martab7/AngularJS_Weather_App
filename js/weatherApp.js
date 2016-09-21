0345604395f30191382dfa1bdf87d81bc0e863db
Anthony <martab7.am@gmail.com>
2016-09-21 18:23:37 -0500

    Updating master with styling while api is down

--- js/weatherApp.js
+++ js/weatherApp.js
@@ -5,59 +5,64 @@
 
     // ------ definitions ---------
     var url = 'http://api.openweathermap.org/data/2.5/forecast'
-    var cities = ['Austin', 'Dallas', 'San Antonio']
+    var cities = ['El Paso', 'Dallas', 'San Antonio', 'Austin']
+    var units = 'imperial'
+    var degree = 'F'
 
     // ------ controller ---------
     app.controller('WeatherController', function ($scope, $http) {
-      $scope.units = 'metric'
-
-      // function weather (city) {
-      //   console.log(city)
-      //   $http.jsonp(url, { params : {
-      //     q : city,
-      //     units : $scope.units,
-      //     appid : '9eee084476f605e9dada18503be885cd',
-      //     callback : 'JSON_CALLBACK'
-      //   }}).success(function (data, status, headers, config) {
-      //     console.log(data)
-      //     return data
-      //   }).error(function () {
-      //     console.log('error')
-      //   })
-      // }
 
       $scope.toggle = function () {
-        if($scope.units == 'metric') {
-          $scope.units = 'imperial'
-        }else if($scope.units == 'imperial') {
-          $scope.units = 'kelvin'
-        }else if ($scope.units == 'kelvin') {
-          $scope.units = 'metric'
+        if(units == 'imperial') {
+          units = 'metric'
+          degree = 'C'
+        }else if(units == 'metric') {
+          units = 'kelvin'
+          degree = 'K'
+        }else if (units == 'kelvin') {
+          units = 'imperial'
+          degree = 'F'
         }
+
+      $scope.select = function (city) {
+        // console.log(city)
+        $http.jsonp(url, { params : {
+          q : city,
+          units : units,
+          appid : '9eee084476f605e9dada18503be885cd',
+          callback : 'JSON_CALLBACK'
+        }}).success(function (data, status, headers, config) {
+          console.log(data.list[0].main.temp)
+          $scope.select = data.city
+          $scope.weather = data.list
+          // console.log($scope.weather)
+        }).error(function () {
+          console.log('error')
+        })
       }
+
 
       $scope.cities = []
       cities.forEach(function (city, index) {
         $http.jsonp(url, { params : {
           q : city,
-          units : $scope.units,
+          units : units,
           appid : '9eee084476f605e9dada18503be885cd',
           callback : 'JSON_CALLBACK'
         }}).success(function (data, status, headers, config) {
-
-        var object = {city: city, temp: data.list[0].main.temp}
+          console.log(data)
+        var object = {city: city, temp: data.list[0].main.temp, sym: degree}
         $scope.cities.push(object)
-      }).error(function () {
+        }).error(function () {
         console.log('error')
+        })
       })
-
-      })
-
+    }
 
   }).directive('myDirective', function () {
     return {
       restrict: "A",
-      template: '<td>{{ myDirective.city }}</td><td>{{ myDirective.temp }}</td>',
+      template: '<td>{{ myDirective.city }}</td><td>{{ myDirective.temp }} {{ myDirective.sym }}</td>',
       scope: {
         myDirective: '='
       },
